7b0a6a89ddff2acf0a1f69af36353deba2446203
oliviatwest <oliviatwest@outlook.com>
2025-07-28 15:46:23 +0100

    checking logo spacing

--- Hangman/main.py
+++ Hangman/main.py
@@ -3,13 +3,18 @@ import artwork
 import wordlist
 
 #logo
-print(artwork.logo)
+print(artwork.logo + "\n")
+
 
 #init variables
 chosen_word = random.choice(wordlist.word_list)
 lives = 6
+game_over = False
+
 
 placeholder = ""
 wordlength = len(chosen_word)
 
-print(chosen_word)
+for position in range(wordlength):
+    placeholder += "_"
+print("Ready? \n" + "Here's your word: " + placeholder)
